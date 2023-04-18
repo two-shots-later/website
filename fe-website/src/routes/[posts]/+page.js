df26c411321bdc5a743677f38acf106480d2d4c0
Gearhartlove <kristoffgearhartlove@gmail.com>
2023-04-17 22:35:36 -0500

    Ignore posts directory for now

--- fe-website/src/routes/[posts]/+page.js
+++ fe-website/src/routes/[posts]/+page.js
@@ -1,27 +1,27 @@
 // Since it's on +page.js, this code will run before page load (likely on server)
 
 // Get posts info
-const allPosts = import.meta.glob('posts/hello.md', { eager: true }); //TODO: fix this directory 
+// const allPosts = import.meta.glob('posts/hello.md', { eager: true }); //TODO: fix this directory 
 
-let posts = [];
-// Get the posts' slugs
-for (let path in allPosts) {
-	const post = allPosts[path];
-	const slug = post.metadata.slug;
-	const p = { post, slug };
-	posts.push(p);
-}
+// let posts = [];
+// // Get the posts' slugs
+// for (let path in allPosts) {
+// 	const post = allPosts[path];
+// 	const slug = post.metadata.slug;
+// 	const p = { post, slug };
+// 	posts.push(p);
+// }
 
-export function load({ params }) {
-	const { slug } = params;
+// export function load({ params }) {
+// 	const { slug } = params;
 
-	// Find the post with the slug
-	const filteredPost = posts.find((p) => {
-		return p.slug.toLowerCase() === slug.toLowerCase();
-	});
+// 	// Find the post with the slug
+// 	const filteredPost = posts.find((p) => {
+// 		return p.slug.toLowerCase() === slug.toLowerCase();
+// 	});
 
-	return {
-		// Tell page to load that post's module
-		page: filteredPost.post.default
-	};
-}
+// 	return {
+// 		// Tell page to load that post's module
+// 		page: filteredPost.post.default
+// 	};
+// }
