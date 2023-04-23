745eda9f166557a876e56bb9b56ab7b637e7a763
Gearhartlove <kristoffgearhartlove@gmail.com>
2023-04-23 16:35:52 -0500

    navigation bar added, reroute / to /games

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,64 @@
 <script>
 	import "../app.css";
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// NAV BAR SCRIPT STUFF
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+	import {page} from '$app/stores';
+	export let black = "text-black";
+	export let selectedOrange = "text-orange-700";
+
+	export let navGamesColor;
+	export let navAboutColor;
+	export let navBlogColor;
+	export let navContactColor;
+	export const urls = new Map([["/games", "text-black"], ["/about", "text-black"], ["/blog", "text-black"], ["/contact", "text-black"]])
+
+	$: {
+		let currentPath = $page.url.pathname
+		
+		urls.forEach(function(_color, url) {
+			if (currentPath.startsWith(url)) {
+				urls.set(url, selectedOrange)
+			} else {
+				urls.set(url, black)
+			}
+		})
+
+		navGamesColor = urls.get("/games")
+		navAboutColor = urls.get("/about")
+		navBlogColor = urls.get("/blog")
+		navContactColor = urls.get("/contact")
+	}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 </script>
+
+<!-- logo and navigation bar, applied to every page -->
+<div class="flex place-content-center">
+	<img
+		src="branding/text_clear_tsl_gun.png"
+		alt="two shots later logo"
+		width="225"
+		height="225"
+		class="child"
+	/>
+</div>
+<div class="flex place-content-center mt-4">
+	<ul>
+		<li class="mr-6 inline">
+			<a href="/games" class="color: {navGamesColor} hover:text-orange-500">Games</a>
+		</li>
+		<li class="mr-6 inline">
+			<a href="/about" class="color: {navAboutColor} hover:text-orange-500">About</a>
+		</li>
+		<li class="mr-6 inline">
+			<a href="/blog" class="color: {navBlogColor} hover:text-orange-500">Blog</a>
+		</li>
+		<li class="mr-6 inline">
+			<a href="/contact" class="color: {navContactColor} hover:text-orange-500">Contact</a>
+		</li>
+	</ul>
+</div>
 
 <main>
 	<slot />
---
+++ src/routes/games/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+    function handleClick() {
+        window.open("https://rustaway.itch.io/the-fantastical-cookbook");
+    }
+    function handleMouseEnter() {}
+    function handleMouseOut() {}
+</script>
+
+<div class="parent">
+    <div class="parent child">
+        <img
+            src="fantastical_cookbook.GIF"
+            alt="goblin standing behind bar with food to the left"
+            class="child game_image"
+            on:click={handleClick}
+        />
+        <a
+            href="https://rustaway.itch.io/the-fantastical-cookbook"
+            style="text-decoration:none"
+            class="text_child"
+            target="_blank"
+        >
+            The Fantastical Cookbook
+        </a>
+        <!-- <h2 class="text_child" cursor="pointer">The Fantastical Cookbook</h2> -->
+        <p class="text_child">Cook fantastical food for friends</p>
+    </div>
+</div>
+
+<style>
+    .parent {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+    .child {
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 10px;
+        margin-top: 7px;
+    }
+    .text_child {
+        justify-content: center;
+        margin: auto;
+    }
+    .game_image {
+        width: 400px;
+        height: 300px;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+    ul#menu li {
+        display: inline;
+    }
+</style>
